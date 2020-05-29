25b946128b563cfa05deac9ef438ed4da084807e
Daniel Andrew <danwandrew@gmail.com>
2020-05-29 08:15:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,7 @@
 
 class Shoe 
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand)
     @brand =brand
@@ -9,6 +10,6 @@ class Shoe
   
   def cobble
     @condition = "new"
-    puts 
+    puts "Your shoe is as good as new!"
   end
 end
